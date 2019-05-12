36561285276456aa91d318d9b70e0a9c5212e7e0
drumzminister <ishukpong418@gmail.com>
2019-05-12 16:40:12 +0100

    updated login page to template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,92 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
 @section('content')
-<div class="container">
-    @if(Session::has('registered'))
-        <div class="row">
-            <div class="col-8 mx-auto">
-                <div class="alert alert-success">
-                    <strong>Success!</strong> Check your email to verify email, then login!
+<main id="main">
+    <div class="enter-block">
+        <div class="container">
+            <div class="form-box">
+                <header class="entry-header">
+                    <h2>Login to your account</h2>
+                </header>
+                <form action="{{route('login')}}" method="POST" class="enter-form login-form">
+                    @csrf
+
+                    <div class="form-group row">
+
+                        <div class="col-md-6 form-row">
+                            <input id="email" type="email"
+                                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                value="{{ old('email') }}" placeholder="Email *" required>
+
+                            @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+
+                        <div class="col-md-6 form-row">
+                            <input id="password" type="password"
+                                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                                placeholder="Password*" required>
+
+                            @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+
+                    <div class="form-group row">
+                        <div class="col-md-6 form-row restore-pass-mod">
+                            <div class="form-check custom-checkbox">
+                                <input class="form-check-input" type="checkbox" name="remember" id="terms"
+                                    {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="terms">
+                                    {{ __('Remember') }}
+                                </label>
+                            </div>
+
+                            <div>
+                                <a href="{{route('password.request')}}">Fogot password?</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                    <div class="help-text">
+                        <span>* Required field</span>
+                    </div>
+                </form>
+                <div class="singup-link-block">
+                    <p>Don't have an account?</p>
+                    <div class="btn-wrap">
+                        <a class="btn btn-outline" href="{{route('register')}}">Sign Up</a>
+                    </div>
+                    <div class="entry-confirm">
+                        <p>By continuing you're confirming that you've read our <a href="#">Terms & Conditions</a> and
+                            <a href="#">Cookie Policy</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        {{Session::forget('registered')}}
-    @endif
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <aside class="enter-aside">
+                <h3><mark>Benefits</mark> of registering with us</h3>
+                <ul>
+                    <li>View and respond to customer reviews </li>
+                    <li>Update your company profile</li>
+                    <li>Generate bage for your website</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+            </aside>
         </div>
     </div>
-</div>
+</main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('auth.logout');
+    return view('welcome');
 });
 
 Auth::routes(['verify' => true]);
